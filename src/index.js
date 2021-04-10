1ca29108cb94895bd5be2982c1af270698bf9267
Michael Holmes <michaelholmes618@gmail.com>
2021-04-10 12:53:57 -0500

    add random number

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Sly";
-//const luckyNumber = Math.floor(Math.random() * 21) + 1;
-const luckyNumber = 7;
-//const paragraph = "Your Lucky number is " + luckyNumber;
+const luckyNumber = Math.floor(Math.random() * 21) + 1;
+
+const paragraph = "Your Lucky number is " + luckyNumber;
 
 //ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById("root"));
 
 ReactDOM.render(
   <div>
     <h1>Hello {name}!</h1>
-    <p>"Your Lucky number is {luckyNumber}.</p>
+    <p>{paragraph}.</p>
   </div>,
   document.getElementById("root")
 );
